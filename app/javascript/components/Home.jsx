ef802af640b0d6703becae700ae90aa380f01c95
Aaron Davis <aaron@heal.me>
2023-01-18 12:14:07 -0700

    add book image and link; improve styling

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -90,30 +90,35 @@ export default () => {
 
   return (
     <div className="max-w-lg mx-auto">
-      <h1 className="font-bold text-xl text-center">Ask My Book</h1>
-      <p className="text-gray-700 text-base">
+      <div className="pt-10">
+        <a href="https://www.amazon.com/Minimalist-Entrepreneur-Great-Founders-More/dp/0593192397" target="_blank">
+          <img className="mx-auto h-64" src="https://askmybook.com/static/book.2a513df7cb86.png" alt="The Minimalist Entrepreneur" />
+        </a>
+      </div>
+      <h1 className="font-bold text-xl text-center mt-5">Ask My Book</h1>
+      <p className="text-gray-700 text-base mt-5">
         This is an experiment in using AI to make my book's content more accessible. Ask a question and I'll answer it
         in real-time:
       </p>
-      <textarea className="w-full border rounded p-2" value={question} onChange={(e) => setQuestion(e.target.value)}/>
-      {!response ? (
-        <div className="text-center py-2">
-          <RoundedButton className="bg-black text-white mr-10" disabled={asking} onClick={() => { onAsk(question) }}>{asking ? 'Asking...' : 'Ask question'}</RoundedButton>
-          <RoundedButton className="bg-gray-200 text-gray-700" disabled={asking} onClick={onFeelingLucky}>I'm feeling lucky</RoundedButton>
-        </div>
-      ) : (
-        <div className="py-2">
-          {response && <b>Answer: </b>}
-          {answer}
-        </div>
-      )}
+      <textarea className="w-full border rounded p-2 mt-5" value={question} onChange={(e) => setQuestion(e.target.value)}/>
+      <div className="mt-5">
+        {!response ? (
+          <div className="text-center">
+            <RoundedButton className="bg-black text-white mr-10" disabled={asking} onClick={() => { onAsk(question) }}>{asking ? 'Asking...' : 'Ask question'}</RoundedButton>
+            <RoundedButton className="bg-gray-200 text-gray-700" disabled={asking} onClick={onFeelingLucky}>I'm feeling lucky</RoundedButton>
+          </div>
+        ) : (
+          <div>
+            {response && <b>Answer: </b>}
+            {answer}
+          </div>
+        )}
+      </div>
       {showAskAnotherButton &&
-        <div className="py-2">
-          <RoundedButton className="bg-black text-white" onClick={() => {
-            resetState()
-            navigate('/')
-          }}>Ask another question</RoundedButton>
-        </div>
+        <RoundedButton className="bg-black text-white mt-5" onClick={() => {
+          resetState()
+          navigate('/')
+        }}>Ask another question</RoundedButton>
       }
     </div>
   )
